057ce7be17a2066860c32d7b6905a154914ab32e
Alex Jones <alex.l.jones.3@gmail.com>
2016-10-19 18:46:06 -0400

    refactored to index router from index.js

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var indexRouter = express.Router();
+var path = require('path');
 
+indexRouter.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: path.resolve('./')});
 
-indexRouter.get('/', function(request, response, next){
-  response.sendFile('../public/html/index.html', {root: __dirname});
-  next();
 });
 
 module.exports = indexRouter;
