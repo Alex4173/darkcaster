971d06a57c28921b4c9346bf57138e92191a5c1e
Alex Jones <alex.l.jones.3@gmail.com>
2016-10-18 20:04:27 -0400

    added auth. middleware

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
-
+var authorize = require('./middleware/auth.js');
 
 var forecastRouter = require('./routers/forecast.router.js');
 
@@ -11,7 +11,7 @@ var port = process.env.PORT || 8080;
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
-
+server.use(authorize);
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
